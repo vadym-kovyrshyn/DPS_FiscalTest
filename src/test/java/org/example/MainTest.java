6fd957ea314a31f23d93166eb9a2499c7b60956c
vadym.kovyrshyn <vadim.kovyrshin@gmail.com>
2025-10-19 01:50:23 +0300

    Init

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+
+    @Test
+    void testMethod() {
+        System.out.println("Hello from test method!");
+    }
+}
